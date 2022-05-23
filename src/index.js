a81038ed2d4f4b5d4d17c94616123f395adc688c
Julia <emidiant1999@gmail.com>
2022-05-23 22:50:30 +0300

    Alaska + Hawaii + multipolygon

--- src/index.js
+++ src/index.js
@@ -1,76 +1,79 @@
 
 var title = d3
-  .select("#content")
-  .append("div")
-  .style("height", "30px")
-  .style("width", "300px")
-  .style("text-align", "center")
-  .style("font-size", "1.8em")
-  .style("margin-left", "auto")
-  .style("margin-right", "auto")
-  .html("Crime in United States");
+    .select("#content")
+    .append("div")
+    .style("height", "30px")
+    .style("width", "300px")
+    .style("text-align", "center")
+    .style("font-size", "1.8em")
+    .style("margin-left", "auto")
+    .style("margin-right", "auto")
+    .html("Crime in United States");
 
-var svg = d3
-  .select("#content")
-  .append("svg")
-  // .style("border-radius", "15px")
-  // .append("g")
-  .attr("width", 800)
-  .attr("height", 400);
+var svg_map = d3
+    .select("#content")
+    .append("svg")
+    .attr("width", 800)
+    .attr("height", 400)
+    .append("g");
 
 // svg.append("svg:image")
-// .attr('x', 70)
-// .attr('y', 30)
-// .attr('width', 655)
-// .attr('opacity', 0.4)
-// .attr("xlink:href", "https://states-of-america.ru/karty-ssha/karty/karta-shtatov-ssha.jpg")
+//     .attr('x', 70)
+//     .attr('y', 30)
+//     .attr('width', 655)
+//     .attr('opacity', 0.4)
+//     .attr("xlink:href", "https://states-of-america.ru/karty-ssha/karty/karta-shtatov-ssha.jpg")
 
 var tooltip = d3
-  .select("#content")
-  .append("div")
-  .style("display", "block")
-  .style("position", "absolute")
-  .style("visibility", "hidden")
-  .style("background-color", "rgba(255, 255, 255, 0.7)")
-  .style("border", "solid")
-  .style("border-width", "1px")
-  .style("border-radius", "5px")
-  .style("padding", "10px");
+    .select("#content")
+    .append("div")
+    .style("display", "block")
+    .style("position", "absolute")
+    .style("visibility", "hidden")
+    .style("background-color", "rgba(255, 255, 255, 0.7)")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px");
 
-// <input type="range" name="mySlider" id=mySlider min="2" max="40" value="10">
+// sliderHorizontal, sliderVertical, sliderTop, sliderRight, sliderBottom, sliderLeft
 
-// var slider = d3.select("#content")
-//   .append("input")
-//   .attr("type", "range")
-//   .attr("name", "year_slider")
-//   .attr("id", "yearSlider")
-//   .attr("min", 2)
-//   .attr("max", 40)
-//   .attr("value", 10)
-//   .style("display", "block")
-//   .style("margin-top", "20px")
-//   .style("margin-left", "auto")
-//   .style("margin-right", "auto")
-//   .style("width", "700px");
+const slider = d3
+    .sliderHorizontal()
+    .min(2001)
+    .max(2016)
+    .value(2008)
+    .step(1)
+    .ticks(16)
+    .width(750)
+    .displayFormat(d3.format(".0f"))
+    .tickFormat(d3.format(".0f"))
+    .on('onchange', (val) => {
+      console.log(val);
+    });
 
-// todo listen slider
-// d3.select("#mySlider").on("change", function(d){
-//   console.log(d);
-//   // selectedValue = this.value
-//   // changeSize(selectedValue)
-// })
+const slider_svg = d3
+    .select("#content")
+    .append("svg")
+    .attr("class", "#mySlider")
+    .attr("width", 800)
+    .attr("height", 80)
+    .append("g")
+    .attr("transform", "translate(30,30)")
+    .call(slider);
 
 var state_div = d3
-  .select("#content")
-  .append("div")
-  .style("height", "30px")
-  .style("width", "200px")
-  .style("text-align", "center")
-  .style("font-size", "1.5em")
-  .style("margin-top", "10px")
-  .style("margin-left", "auto")
-  .style("margin-right", "auto")
-  .html("National");
+    .select("#content")
+    .append("div")
+    .attr("id", "state_div")
+    .style("height", "30px")
+    .style("width", "200px")
+    .style("text-align", "center")
+    .style("font-size", "1.5em")
+    .style("margin-top", "15px")
+    .style("margin-left", "auto")
+    .style("margin-right", "auto")
+    .text("National");
 
 var mouseover = function (d) {
   tooltip.style("visibility", "visible");
@@ -106,43 +109,80 @@ var state_click = function (d) {
 
 // почему-то ссылка побилась
 // link = "https://raw.githubusercontent.com/Emidiant/crime-in-usa-visualisation/main/coordinates_extraction/state_coordinates/csv/polygon.csv"
-link = "https://rawcdn.githack.com/Emidiant/crime-in-usa-visualisation/f16a19f937d3f8fa6fccaa046e924715dd3cbcde/coordinates_extraction/state_coordinates/csv/polygon.csv"
+link = "https://raw.githubusercontent.com/Emidiant/crime-in-usa-visualisation/main/coordinates_extraction/state_coordinates/csv/polygon.csv"
+
 d3.csv(link, function (data) {
   var state_name = data.state;
   var state_points = JSON.parse(data.new_coordinates);
+  if (state_name !== "Alaska" && state_name !== "Hawaii") {
+    if (data.type !== "multipolygon") {
+      svg_map
+        .append("polyline")
+        // .data(state_name)
+        .text(data.state)
+        .style("fill", "orange")
+        .attr("stroke", "grey")
+        .attr("opacity", 0.6)
+        .attr("points", state_points)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("click", state_click)
+        .on("mouseleave", mouseleave);
+      } else {
+      console.log("multi", state_name)
+      for (var i = 0; i < state_points.length; i += 1) {
+        svg_map
+            .append("polyline")
+            // .data(state_name)
+            .text(data.state)
+            .style("fill", "green")
+            .attr("stroke", "grey")
+            .attr("opacity", 0.6)
+            .attr("points", state_points[i])
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("click", state_click)
+            .on("mouseleave", mouseleave);
+      }
+    }
+  } else {
+    console.log("state", state_name)
+    if (state_name === "Alaska") {
+          for (var i = 0; i < state_points.length; i += 1) {
+        svg_map
+            .append("polyline")
+            // .data(state_name)
+            .text(data.state)
+            .style("fill", "green")
+            .attr("stroke", "grey")
+            .attr("opacity", 0.6)
+            .attr("points", state_points[i])
+            .attr("transform", "translate(-90,-1070)")
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("click", state_click)
+            .on("mouseleave", mouseleave);
+      }
+    } else {
+        for (var i = 0; i < state_points.length; i += 1) {
+        svg_map
+            .append("polyline")
+            // .data(state_name)
+            .text(data.state)
+            .style("fill", "green")
+            .attr("stroke", "grey")
+            .attr("opacity", 0.6)
+            .attr("points", state_points[i])
+            .attr("transform", "translate(680, 250)")
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("click", state_click)
+            .on("mouseleave", mouseleave);
+      }
+    }
+  }
 
-  svg
-    .append("polyline")
-    .data(state_name)
-    .text(data.state)
-    .style("fill", "orange")
-    .attr("stroke", "grey")
-    .attr("opacity", 0.6)
-    .attr("points", state_points)
-    .on("mouseover", mouseover)
-    .on("mousemove", mousemove)
-    .on("click", state_click)
-    .on("mouseleave", mouseleave);
 });
 
-// sliderHorizontal, sliderVertical, sliderTop, sliderRight, sliderBottom, sliderLeft
-
-const slider = d3
-  .sliderBottom()
-  .min(2004)
-  .max(2017)
-  .step(1)
-  .width(700)
-  .displayFormat(d3.format(".0f"))
-  .tickFormat(d3.format(".0f"));
-
-const g = d3
-  .select("#content")
-  .append("svg")
-  .attr("class", "#mySlider")
-  .attr("width", 800)
-  .attr("height", 80)
-  .append("g")
-  .attr("transform", "translate(30,30)");
-
-g.call(slider);
+// перемещение карты после отрисовки
+svg_map.attr("transform", "translate(-20,-40)")
